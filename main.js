d9fa87cb3071980eec0cfe10fdbd1ffacaad0044
silvaryas <ryanalvesdasilva40@gmail.com>
2024-09-27 09:05:45 -0300

    cv

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ function soma (num1,num2){
 }
 console.log(soma(5,3));
 
-function área (num1,num2){
-    return num1*num2
-
+function areadeumquadrado(lado){
+    return lado**2
 }
+console.log(areadeumquadrado(6));
 
+let media=function(media){
+    return 1+2+3/3;
+}
 
 
 
